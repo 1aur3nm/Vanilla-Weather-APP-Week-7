d36a79a66fbddb214e9a6969bb2d0bd2eea5f6fd
1aur3nm <102338016+1aur3nm@users.noreply.github.com>
2022-07-11 14:09:35 +1000

    Updated date and time elements

--- Vanilla_Weather_App/app.js
+++ Vanilla_Weather_App/app.js
@@ -1,3 +1,27 @@
+function formatDateAndTime(timestamp) {
+  let date = new Date(timestamp);
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let mins = date.getMinutes();
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${mins}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#number-temp");
@@ -8,6 +32,9 @@ function displayTemperature(response) {
 
   let conditionsElement = document.querySelector("#conditions");
   conditionsElement.innerHTML = response.data.weather[0].decription;
+
+  let dateElement = document.querySelector("#dateAndTime");
+  dateElement.innerHTML = formatDateAndTime(response.data.dt * 1000);
 }
 
 let apiKey = "9c9080e190f8de94f431567b5176425a";
